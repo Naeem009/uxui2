c90e5ff0825bde6a5a1f6e4f07a877fae82209d1
Naeem009 <56760480+Naeem009@users.noreply.github.com>
2020-07-07 21:25:43 +0500

    new files of JS updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import NavBar from './Components/NavBar.js';
 import FullWidthGrid from './Components/Grids.js';
+import FootNav from './Components/FooterNav.js';
 
 function App() {
+  
+  const screenConfig = useState(0);
   return (
     <div className="App">
       <NavBar />
-      <FullWidthGrid/>
+      <FullWidthGrid currentScreen = {screenConfig[0]}/>
+      <FootNav screenConfig={screenConfig}/>
     </div>
   );
 }
